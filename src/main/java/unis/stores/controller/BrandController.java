1a08e611d1a61e58a4f2e2d162d62f56eb05b89b
Javier Alvarez <calvarez098@gmail.com>
2020-09-01 17:50:53 -0600

    error de compilacion

--- src/main/java/unis/stores/controller/BrandController.java
+++ src/main/java/unis/stores/controller/BrandController.java
@@ -31,7 +31,7 @@ public class BrandController {
      * @return    returns the result of the creation of the brand
      */
     @PostMapping("/brand")
-    public ResponseEntity<Object> create(@RequestBody Map<String, String> body) {
+    public create(@RequestBody Map<String, String> body) {
         if (!body.containsKey(Constants.BRAND_NAME_LABEL))
             return ResponseEntity.badRequest().body(new CreateBrandResult(false, "Bad Request", null));
 
